4fa6fc469ab358865c2d3bbf903c0ff4a2ff98cc
Linnore <junleli@link.cuhk.edu.cn>
2022-12-04 14:35:19 +0800

    Developing SVD for singular matrices

--- utils/SVD.py
+++ utils/SVD.py
@@ -17,7 +17,6 @@ def svd_phaseI(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         B (np.ndarray): The bidiagonalized version of A, with dimension min(m,n) x min(m, n).
         Qt (np.ndarray), and P (np.ndarray): The two transformation matrices such that B = Qt @ A @ P
     """
-    B = np.zeros(A.shape)
     m, n = A.shape
     flag_T = False
     if m < n:
@@ -25,64 +24,79 @@ def svd_phaseI(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         A = A.T
         m, n = A.shape
 
+    B = A.copy()
     i = 0
-    Qt = HouseHolder(A[:, 0])
-    A = Qt @ A
+    Qt = HouseHolder(B[:, 0])
+    B = Qt @ B
 
     P = np.zeros((n, n))
     P[0, 0] = 1
-    P[1:, 1:] = HouseHolder(A[0, 1:].T)
-    A[:, 1:] = A[:, 1:] @ P[1:, 1:]
+    P[1:, 1:] = HouseHolder(B[0, 1:].T)
+    B[:, 1:] = B[:, 1:] @ P[1:, 1:]
 
     for i in range(1, n-1):
-        Qit = HouseHolder(A[i:, i])
-        A[i:, i:] = Qit @ A[i:, i:]
+        Qit = HouseHolder(B[i:, i])
+        B[i:, i:] = Qit @ B[i:, i:]
 
-        Pi = HouseHolder(A[i, (i+1):].T)
-        A[i:, i+1:] = A[i:, i+1:] @ Pi
+        Pi = HouseHolder(B[i, (i+1):].T)
+        B[i:, i+1:] = B[i:, i+1:] @ Pi
 
         Qt[i:, :] = Qit @ Qt[i:, :]
         P[:, i+1:] = P[:, i+1:] @ Pi
 
     i = n-1
-    Qit = HouseHolder(A[i:, i])
-    A[i:, i:] = Qit @ A[i:, i:]
+    Qit = HouseHolder(B[i:, i])
+    B[i:, i:] = Qit @ B[i:, i:]
     Qt[i:, :] = Qit @ Qt[i:, :]
 
     if flag_T:
-        B = A.T
+        B = B.T
         return B, P.T, Qt.T
     else:
-        B = A
         return B, Qt, P
 
 
-def svd_phaseIIA(B: np.ndarray, Qt: np.ndarray, P: np.ndarray, eigenTest=False) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+def svd_phaseIIA(B: np.ndarray, Qt: np.ndarray, P: np.ndarray, eigen=eigh_by_QR) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """This function implement the phaseII of SVD following the proposed procedure A in project description.
+
+    Args:
+        B (np.ndarray): The bidiagonalized version of A such that A = Qt @ B @ P
+        Qt (np.ndarray): An orthogonal matrix that performs the left Householder transformation on A.
+        P (np.ndarray): An orthogonal matrix that performs the rigth Householder transformation on A.
+        eigen (function, optional): The eigen solver used to get eigenvalues and eigen vectors of B @B.T or B.T @ B. 
+            Defaults to the one we implement: eigh_by_QR. Possible candidates could be scipy.linalg.eigh,
+            sicpy.linalg.eigh_tridiagonal.
+
+    Returns:
+        U (np.ndarray): The matrix containing left singular vectors of A as columns 
+    """
 
     # Discard the zero rows or columns
     m, n = B.shape
     if (m > n):
         B = B[:n, :]
         Qt = Qt[:n, :]
+        m = n
     elif (m < n):
         B = B[:, :m]
         P = P[:, :m]
+        n = m
 
-    if eigenTest:
-        T, S = scipy.linalg.eigh(B.T@B)
-        T, G = scipy.linalg.eigh(B@B.T)
-    else:
-        T, S = eigh_by_QR(B.T@B)
-        T, G = eigh_by_QR(B@B.T)
+    # Eigen decomposition of B'@B = S @ T @ S'
+    T, G = eigen(B@B.T)
+
+    # For B@B' = G @ T @ G', solve S by B @ S = G @ T
+    # Todo: 
+    T, S = eigen(B.T@B)
 
     U = Qt.T @ G
     Vt = S.T @ P.T
     return U, T**.5, Vt
 
 
-def svd(A: np.ndarray, phaseII=svd_phaseIIA, eigenTest=False) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+def svd(A: np.ndarray, phaseII=svd_phaseIIA, eigen=eigh_by_QR) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     B, Q, P = svd_phaseI(A)
-    return phaseII(B, Q, P, eigenTest=eigenTest)
+    return phaseII(B, Q, P, eigen=eigen)
 
 
 if __name__ == "__main__":
