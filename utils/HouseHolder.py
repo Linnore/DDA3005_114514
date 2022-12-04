e6c239083c7d7cccd148646d15a964790c42bf47
Linnore <junleli@link.cuhk.edu.cn>
2022-12-04 21:12:57 +0800

    Buggy

--- utils/HouseHolder.py
+++ utils/HouseHolder.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def HouseHolder(a: np.ndarray, tol=1e-16) -> np.ndarray:
+def HouseHolder(a: np.ndarray, tol=1e-15) -> np.ndarray:
     """This function implement the HouseHolder transformation on a given vector 'a'.
 
     Args:
@@ -12,9 +12,9 @@ def HouseHolder(a: np.ndarray, tol=1e-16) -> np.ndarray:
     """
     alpha = np.linalg.norm(a)
     n = a.shape[0]
-    if abs(alpha) < tol:
-        # todo: permutation
-        return np.identity(n)
+    # if abs(alpha) < tol:
+    #     # todo: permutation
+    #     return np.identity(n)
 
 
     # To minuate cancellation effects
--- utils/SVD.py
+++ utils/SVD.py
@@ -19,6 +19,32 @@ def svd_phaseI(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     m, n = A.shape
 
+    # Naive way:
+    # B = A.copy()
+    # Qt = np.identity(m)
+    # P = np.identity(n)
+
+    # r = min(m, n)
+    # for i in range(r-1):
+    #     Qit = np.identity(m)
+    #     Qit[i:, i:] = HouseHolder(B[i:, i])
+    #     B = Qit @ B
+
+    #     Pi = np.identity(n)
+    #     Pi[i+1:, i+1: ] = HouseHolder(B[i, i+1:])
+    #     B = B @ Pi
+
+    #     Qt = Qit @ Qt
+    #     P = P @ Pi
+        
+    # if m>n:
+    #     i = n-1
+    #     Qit = np.identity(m)
+    #     Qit[i:, i:] = HouseHolder(B[i:, i])
+    #     B = Qit @ B
+    #     Qt = Qit @ Qt
+
+
     B = A.copy()
     i = 0
     Qt = HouseHolder(B[:, 0])
@@ -34,21 +60,22 @@ def svd_phaseI(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         Qit = HouseHolder(B[i:, i])
         B[i:, i:] = Qit @ B[i:, i:]
 
-        Pi = HouseHolder(B[i, (i+1):].T)
+        Pi = HouseHolder(B[i, (i+1):])
         B[i:, i+1:] = B[i:, i+1:] @ Pi
 
         Qt[i:, :] = Qit @ Qt[i:, :]
         P[:, i+1:] = P[:, i+1:] @ Pi
 
-    i = r-1
-    Qit = HouseHolder(B[i:, i])
-    B[i:, i:] = Qit @ B[i:, i:]
-    Qt[i:, :] = Qit @ Qt[i:, :]
+    if m > n:
+        i = r-1
+        Qit = HouseHolder(B[i:, i])
+        B[i:, i:] = Qit @ B[i:, i:]
+        Qt[i:, :] = Qit @ Qt[i:, :]
 
     return B, Qt, P
 
 
-def svd_phaseIIA(B: np.ndarray, Qt: np.ndarray, P: np.ndarray, eigen=eigh_by_QR) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+def svd_phaseIIA(B: np.ndarray, Qt: np.ndarray, P: np.ndarray, eigen=eigh_by_QR, tol=1e-15) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """This function implement the phaseII of SVD following the proposed procedure A in project description.
 
     Args:
@@ -58,6 +85,7 @@ def svd_phaseIIA(B: np.ndarray, Qt: np.ndarray, P: np.ndarray, eigen=eigh_by_QR)
         eigen (function, optional): The eigen solver used to get eigenvalues and eigen vectors of B @B.T or B.T @ B. 
             Defaults to the one we implement: eigh_by_QR. Possible candidates could be scipy.linalg.eigh,
             sicpy.linalg.eigh_tridiagonal.
+        tol (float): The tolerance for judging zero.
 
     Returns:
         U (np.ndarray): The matrix containing left singular vectors of A as columns 
@@ -67,30 +95,31 @@ def svd_phaseIIA(B: np.ndarray, Qt: np.ndarray, P: np.ndarray, eigen=eigh_by_QR)
 
     # Discard the zero rows or columns
     m, n = B.shape
-    if (m > n):
-        B = B[:n, :]
-        Qt = Qt[:n, :]
-        m = n
-    elif (m < n):
+    if m < n:
         B = B[:, :m]
         P = P[:, :m]
         n = m
+    elif m > n:
+        B = B[:n]
+        Qt = Qt[:n]
+        m = n
 
     # Eigen decomposition of B'@B = S @ T @ S'
+    # Eigen decomposition of B@B' = G @ T @ G'
     T, G = eigen(B@B.T)
-
-    # For B@B' = G @ T @ G', solve S by B @ S = G @ T
-    # Todo: 
-    T, S = eigen(B.T@B)
+    _, S = eigen(B.T@B)
+    # Todo: Save one eigen decomposition
+    # How to apple rowwise backward and forward substitution on singular B?
 
     U = Qt.T @ G
     Vt = S.T @ P.T
+    # T[np.abs(T) < tol] = 0
     return U, T**.5, Vt
 
 
 def svd(A: np.ndarray, phaseII=svd_phaseIIA, eigen=eigh_by_QR) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    B, Q, P = svd_phaseI(A)
-    return phaseII(B, Q, P, eigen=eigen)
+    B, Qt, P = svd_phaseI(A)
+    return phaseII(B, Qt, P, eigen=eigen)
 
 
 if __name__ == "__main__":
