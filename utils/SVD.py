ce934e35ac1d53eb126cf7cfb6df9e285c9fb780
Linnore <junleli@link.cuhk.edu.cn>
2022-12-21 11:07:00 +0800

    phaseI fixed

--- utils/SVD.py
+++ utils/SVD.py
@@ -2,7 +2,7 @@
 """
 
 import numpy as np
-from .HouseHolder import HouseHolder
+from .HouseHolder import HouseHolder, HouseHolder_update
 from .QR import *
 from time import time
 import sys
@@ -11,42 +11,45 @@ sys.setrecursionlimit(4500)
 
 def svd_phaseI(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """This function implement the Golub-Kahan bidiagonalization to reduce the matrix A to bidiagonal form.
+    Note that this input matrix must be a tall matrix, i.e. m>=n.
 
     Args:
         A (np.ndarray): The matrix A whose SVD is of our interest.
 
     Returns:
-        B (np.ndarray): The bidiagonalized version of A, with dimension min(m,n) x min(m, n).
-        Qt (np.ndarray), and P (np.ndarray): The two transformation matrices such that B = Qt @ A @ P
+        B (np.ndarray): The bidiagonalized version of A, with dimension n x n.
+        Qt (np.ndarray), and P (np.ndarray): The two transformation matrices such that B = Qt @ A @ P,
+            where Qt is m by m, P is n by n.
     """
     m, n = A.shape
     B = A.copy()
-    i = 0
-    Qt = HouseHolder(B[:, 0])
-    B = Qt @ B
 
+    Qt = np.identity(m)
+    P = np.identity(n)
     r = min(m, n)
-    P = np.zeros((n, n))
-    P[0, 0] = 1
-    P[1:, 1:] = HouseHolder(B[0, 1:].T)
-    B[:, 1:] = B[:, 1:] @ P[1:, 1:]
 
-    for i in range(1, r):
-        Qit = HouseHolder(B[i:, i])
-        B[i:, i:] = Qit @ B[i:, i:]
+    for i in range(r-1):
+        w, alpha = HouseHolder(B[i:, i])
+        w = w.reshape(-1,1)
+        HouseHolder_update(B[i:, i:], w, alpha)
+        Qt[i:, :] = Qt[i:, :] - w @ w.T @ Qt[i:, :]
 
-        Pi = HouseHolder(B[i, (i+1):])
-        B[i:, i+1:] = B[i:, i+1:] @ Pi
-
-        Qt[i:, :] = Qit @ Qt[i:, :]
-        P[:, i+1:] = P[:, i+1:] @ Pi
+        w, alpha = HouseHolder(B[i, i+1:])
+        w = w.reshape(-1,1)
+        HouseHolder_update(B[i:, i+1:].T, w, alpha)
+        P[:, i+1:] = P[:, i+1:] - P[:, i+1:] @ w @ w.T
 
     i = r-1
-    Qit = HouseHolder(B[i:, i])
-    B[i:, i:] = Qit @ B[i:, i:]
-    Qt[i:, :] = Qit @ Qt[i:, :]
+    v, alpha = HouseHolder(B[i:, i])
+    if alpha == 0:
+        w = v.reshape((-1, 1))
+    else:
+        w = (np.sqrt(2)/norm(v) * v).reshape((-1, 1))
+    HouseHolder_update(B[i:, i:], w, alpha)
+    Qt[i:, :] = Qt[i:, :] - w @ (w.T @ Qt[i:, :])
 
     return B, Qt, P
+    # Todo, form Q can be even faster!
 
 
 def fastMult_upper_bidiagonal(A: np.ndarray, B: np.ndarray) -> np.ndarray:
@@ -142,7 +145,6 @@ def svd(A: np.ndarray, phaseII='Default', eigen=eigh_by_QR) -> tuple[np.ndarray,
     m, n = A.shape
     flipped = False
     if m < n:
-        print("Flipped!")
         flipped = True
         A = A.T
         m, n = A.shape
@@ -171,3 +173,44 @@ def svd(A: np.ndarray, phaseII='Default', eigen=eigh_by_QR) -> tuple[np.ndarray,
         return Vt.T, S, U.T
     else:
         return U, S, Vt
+
+
+# def svd_phaseI(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+#     """This function implement the Golub-Kahan bidiagonalization to reduce the matrix A to bidiagonal form.
+
+#     Args:
+#         A (np.ndarray): The matrix A whose SVD is of our interest.
+
+#     Returns:
+#         B (np.ndarray): The bidiagonalized version of A, with dimension min(m,n) x min(m, n).
+#         Qt (np.ndarray), and P (np.ndarray): The two transformation matrices such that B = Qt @ A @ P
+#     """
+#     m, n = A.shape
+#     B = A.copy()
+#     i = 0
+#     Qt = HouseHolder(B[:, 0])
+#     B = Qt @ B
+
+#     r = min(m, n)
+#     P = np.zeros((n, n))
+#     P[0, 0] = 1
+#     P[1:, 1:] = HouseHolder(B[0, 1:].T)
+#     B[:, 1:] = B[:, 1:] @ P[1:, 1:]
+
+#     for i in range(1, r-1):
+#         Qit = HouseHolder(B[i:, i])
+#         # print(np.sum(np.abs(Qit-Qit.T) < 1e-14)==B[i:, i].shape[0]**2)
+#         B[i:, i:] = Qit @ B[i:, i:]
+
+#         Pi = HouseHolder(B[i, (i+1):])
+#         B[i:, i+1:] = B[i:, i+1:] @ Pi
+
+#         Qt[i:, :] = Qit @ Qt[i:, :]
+#         P[:, i+1:] = P[:, i+1:] @ Pi
+
+#     i = r-1
+#     Qit = HouseHolder(B[i:, i])
+#     B[i:, i:] = Qit @ B[i:, i:]
+#     Qt[i:, :] = Qit @ Qt[i:, :]
+
+#     return B, Qt, P
